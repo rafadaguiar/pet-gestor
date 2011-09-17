32f1dfba4b58fcd89116392178b6b8d91a993503
Juliana Lucena <julianalucenaa@gmail.com>
2011-09-17 09:41:10 -0300

    Criação do projeto Rails (versão 3.0.7).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetGestor::Application.config.secret_token = 'b39b1cbfcdc820e63722edbfa80c492289f3c702eb45f618b803f0c6a65cda4b1715abc2267ad39346c94383ad50e46c7a15bf3ecfec73ef90d360abc7894cfb'
